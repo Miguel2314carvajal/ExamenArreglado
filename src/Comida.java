fb986ac60626e79e17de96f07922ab205adf4106
Miguel Carvajal <miguel.carvajal@epn.edu.ec>
2024-01-03 20:12:05 -0500

    Primer comentario

---
+++ src/Comida.java
@@ -0,0 +1,31 @@
+package src;
+
+public class Comida {
+    //Atributos
+    public String Comida;
+    public Double Precio;
+    public Comida (String Comida,Double Precio){
+        this.Comida = Comida;
+        this.Precio = Precio;
+    }
+
+    public String getComida() {
+        return Comida;
+    }
+
+    public void setComida(String comida) {
+        Comida = comida;
+    }
+
+    public Double getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(Double precio) {
+        Precio = precio;
+    }
+
+    public void MostrarInformacion(){
+        System.out.println("La comida es: "+ Comida + " y su precio es de "+ Precio);
+    }
+}
---
+++ src/Restaurante.java
@@ -0,0 +1,16 @@
+package src;
+
+public class Restaurante extends Comida{
+    //Atributos
+    public String Locacion;
+    public Restaurante(String Comida,Double Precio,String Locacion){
+        super(Comida,Precio);
+        this.Locacion = Locacion;
+    }
+    @Override
+    public void MostrarInformacion(){
+        super.MostrarInformacion();
+        System.out.println("La locación del restaurante es: "+ Locacion);
+    }
+}
+
